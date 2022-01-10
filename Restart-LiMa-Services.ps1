ca89f509d0eb358be0420668363f70fdb9c77351
Erik Sauerbier <erik.sauerbier@mailbox.org>
2022-01-10 17:05:50 +0100

    10.01.22

---
+++ Restart-LiMa-Services.ps1
@@ -0,0 +1,25 @@
+﻿# Skript zum Neustarten bei LiMa-Problemen
+# Stannek GmbH - Version 1.0 - 09.12.2021 ES
+
+#Parameter
+$ASPEnvironment = $True
+
+# Dienste stoppen
+Stop-Service "DATEV Sicherheitspaket-Basis-Dienst"
+Stop-Service "DATEV LiMa-Server"
+Stop-Service "Datev.CC.Processes.Hosting.RdtServiceMode"
+
+# Dienste für ASP Umgebungen stoppen/starten
+if ($ASPEnvironment = $True) 
+    {Stop-Service "DBSZClient"
+     Start-Sleep -Seconds 15
+     Start-Service "DBSZClient"
+    }
+
+# Dienste starten
+Start-Sleep -Seconds 15
+Start-Service "DATEV Sicherheitspaket-Basis-Dienst"
+Start-Sleep -Seconds 15
+Start-Service "DATEV LiMa-Server"
+Start-Sleep -Seconds 15
+Start-Service "Datev.CC.Processes.Hosting.RdtServiceMode"
---
+++ Set-ViwasScheduleScans.ps1
@@ -0,0 +1,29 @@
+﻿# Skript zum Einstellen der Viwas Prüfungen
+# STannek GmbH - v.1.01 - E.Sauerbier
+
+# Registrypfade
+$RegPathQS = "HKLM:\SOFTWARE\WOW6432Node\DATEV Viwas\StandardTasks\QuickScan"
+$RegPathNS = "HKLM:\SOFTWARE\WOW6432Node\DATEV Viwas\StandardTasks\NormalScan"
+$RegItemQS = "Schedule"
+$RegItemNS = "Schedule"
+
+# Zeit für QuickScan generieren
+$RandomHourQS = Get-Random -Minimum 19 -Maximum 23
+$RandomHourQS = $RandomHourQS.ToString()
+$RandomMinuteQS = Get-Random -Minimum 01 -Maximum 58
+$RandomMinuteQS = $RandomMinuteQS.ToString()
+$RandomTimeQS = $RandomHourQS.PadLeft(2,"0")+":"+$RandomMinuteQS.PadLeft(2,"0")
+
+# Zeit für NormalScan generieren
+$RandomHourNS = Get-Random -Minimum 17 -Maximum 23
+$RandomHourNS = $RandomHourNS.ToString()
+$RandomMinuteNS = Get-Random -Minimum 02 -Maximum 58
+$RandomMinuteNS = $RandomMinuteNS.ToString()
+$RandomTimeNS = $RandomHourNS.PadLeft(2,"0")+":"+$RandomMinuteNS.PadLeft(2,"0")
+$RandomDayNS = Get-Random -InputObject 'NNNNNNY','YNNNNNN'
+
+# Viwas Quickscan anpassen
+Set-ItemProperty -Path $RegPathQS -Name $RegItemQS -Value "NYYYNYN,$RandomTimeQS"
+
+# Viwas Normalscan anpassen
+Set-ItemProperty -Path $RegPathNS -Name $RegItemNS -Value "$RandomDayNS,$RandomTimeNS"
